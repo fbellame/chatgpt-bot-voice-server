eb214f11b687cd44092cde6952a73a6bcda27885
Farid Bellameche <faridautomatic@gmail.com>
2023-03-04 21:07:00 -0500

    replace say mac os command to gTTS

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import os
 import openai
 import gradio as gr
-import subprocess
+#import IPython.display as ipd
+from gtts import gTTS
+import io
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("MY_ORGANISATION")
-messages = [{"role": "system", "content": "Tu es un assistant. Répond à la manière d'un thérapeute."}]
+messages = [{"role": "system", "content": "Répond en faisant des réponses courtes."}]
 
 
 def bot_ai(audio):
@@ -24,14 +26,21 @@ def bot_ai(audio):
     system_message = response["choices"][0]["message"]
     messages.append(system_message)
 
-    subprocess.call(["say", system_message["content"]])
+    reponse_file = "reponse-gpt3.mp3"
+    tts = gTTS(text=system_message["content"], lang= 'fr', slow=False, tld="fr")
+    tts.save(reponse_file)
 
     chat_transcript = ""
     for message in messages:
         if message["role"] != "system":
             chat_transcript += message["role"] + ":" + message["content"] + "\n\n"
 
-    return chat_transcript
+    return reponse_file, chat_transcript
 
 if __name__ == "__main__":
-    ui = gr.Interface(fn=bot_ai, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+
+    outputs = [gr.Audio(label="Output Audio", type="filepath"), "text"]
+    
+    ui = gr.Interface(fn=bot_ai, inputs=gr.Audio(source="microphone", type="filepath"), outputs=outputs )
+    shared_url = ui.launch('share=True')
+    print(shared_url)
